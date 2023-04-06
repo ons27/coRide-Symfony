e97c004faf88deedf14c04414041670354d7970b
Saif Yahyaoui <saif.yahyaoui@esprit.tn>
2023-04-06 10:47:13 +0100

    deuxieme commit

---
+++ src/Entity/Trajet.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TrajetRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TrajetRepository::class)]
+class Trajet
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $depart = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $destination = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDepart(): ?string
+    {
+        return $this->depart;
+    }
+
+    public function setDepart(string $depart): self
+    {
+        $this->depart = $depart;
+
+        return $this;
+    }
+
+    public function getDestination(): ?string
+    {
+        return $this->destination;
+    }
+
+    public function setDestination(string $destination): self
+    {
+        $this->destination = $destination;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
